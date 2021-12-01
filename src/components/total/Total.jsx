6088bf3fe441f4220588b70745132bd7e4fb106c
rinalditomas <rinalditomas@gmail.com>
2021-12-01 07:35:21 +0000

    delete function added

--- src/components/total/Total.jsx
+++ src/components/total/Total.jsx
@@ -2,25 +2,22 @@ import React, { useEffect, useState } from "react";
 import './Total.css'
 
 const Total = ({ total }) => {
-  if(total != 0){
-    const { totalIncome, totalExpense } = total[0] 
-    const totalMoney = ()=>{
-        return totalIncome - totalExpense
-    }
+  const { totalIncome, totalExpense } = total;
+  const totalMoney = ()=>{
+      return totalIncome - totalExpense
   }
- 
   return (
     <div className="t-container">
       <div className="container ">
-        <h1 className='incomes'>{`$${totalIncome || total}`}</h1>
+        <h1 className='incomes'>{`$${totalIncome}`}</h1>
       </div>
       <div className="container ">
-        <h1 className='expenses'>{`$${totalExpense || total}`}</h1>
+        <h1 className='expenses'>{`$${totalExpense}`}</h1>
       </div>
 
       <div className={totalMoney() > 0 ? "container total green": "container total red"}>
         <h1>Total:</h1>
-        <h1>{`$${(totalIncome - totalExpense)|| total}`}</h1>
+        <h1>{`$${totalIncome - totalExpense}`}</h1>
       </div>
     </div>
   );
